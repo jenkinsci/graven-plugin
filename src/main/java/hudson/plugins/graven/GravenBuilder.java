4c93d1e0734566ec2d4e324e838eb477d1573623
Nicolas De Loof <nicolas.deloof@gmail.com>
2011-10-07 18:39:42 +0200

    manage GRaveN@Cloud integration as a global option

--- src/main/java/hudson/plugins/graven/GravenBuilder.java
+++ src/main/java/hudson/plugins/graven/GravenBuilder.java
@@ -7,6 +7,9 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import net.sf.json.JSONObject;
 
 /**
  * @author <a href="mailto:nicolas.deloof@cloudbees.com">Nicolas De loof</a>
@@ -24,12 +27,48 @@ public class GravenBuilder extends Builder {
         this.task = task;
     }
 
+
+    public boolean isEnableCloud() {
+        return getDescriptor().isEnableCloud();
+    }
+
+    @Override
+    public Descriptor getDescriptor() {
+        return (Descriptor) super.getDescriptor();
+    }
+
     @Extension
     public static class Descriptor extends BuildStepDescriptor<Builder> {
+
+        private boolean enableCloud;
+
+        public Descriptor() {
+            load();
+        }
+
+        public boolean isEnableCloud() {
+            return enableCloud;
+        }
+
+        public void setEnableCloud(boolean enableCloud) {
+            this.enableCloud = enableCloud;
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            req.bindJSON(this, json.getJSONObject("graven"));
+            save();
+            return true;
+        }
 
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> jobType) {
             return FreeStyleProject.class.isAssignableFrom(jobType);
+        }
+
+        @Override
+        public Builder newInstance(StaplerRequest req, JSONObject formData) throws FormException {
+            return super.newInstance(req, formData);    //To change body of overridden methods use File | Settings | File Templates.
         }
 
         @Override
